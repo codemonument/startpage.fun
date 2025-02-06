b9293f79eb41c58c2f68623bb3d661a79173ca49
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-02-06 13:16:29 +0100

    sdd db seed

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,18 @@
-import { defineDb } from 'astro:db';
+import {column, defineDb, defineTable} from 'astro:db';
+
+/**
+ * Stores links to external websites.
+ */
+const Link = defineTable({
+	columns: {
+		id: column.number({primaryKey: true}),
+		url: column.text(),
+		title: column.text(),
+		image: column.text(),
+	},
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+	tables: {Link},
 });
