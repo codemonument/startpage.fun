9a2550e4e827b753bd5ada38f71e78205870bb0d
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-02-06 12:20:33 +0100

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import solidJs from '@astrojs/solid-js';
 
 import netlify from '@astrojs/netlify';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  adapter: netlify()
+  adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
