756a30b3f87ddecd2450b1960750c49ec6a27eb9
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-02-06 12:18:12 +0100

    bun astro-add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  adapter: netlify()
 });
